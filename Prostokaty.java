63d62c88f7175624d321de0a1d3b85d57982476c
MikeS <97959151+MikeSz99@users.noreply.github.com>
2022-04-04 13:08:54 +0200

    Add files via upload

---
+++ Prostokaty.java
@@ -0,0 +1,28 @@
+package com.company;
+import javax.sound.midi.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class Prostokaty extends JPanel implements ControllerEventListener {
+
+    boolean komunikat = false;
+    public void controlChange(ShortMessage zdarzenie) {
+        komunikat = true;
+        repaint();
+    }
+    public void paintComponent(Graphics g) {
+        if (komunikat) {
+            Graphics2D g2 = (Graphics2D) g;
+            int c = (int) (Math.random() * 250);
+            int z = (int) (Math.random() * 250);
+            int n = (int) (Math.random() * 250);
+            g.setColor(new Color(c,z,n));
+            int wys = (int) ((Math.random() * 320) + 70);
+            int szer = (int) ((Math.random() * 320) + 70);
+            int x = (int) ((Math.random() * 100) + 30);
+            int y = (int) ((Math.random() * 100) + 30);
+            g.fillRect(x,y,wys,szer);
+            komunikat = false;
+        } // koniec if
+    } // koniec metody
+}
